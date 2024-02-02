6a6e6ce6a9bfe08507dbed44a212fae1638bff11
Tijs Zwinkels <tijs@tinkertank.eu>
2024-02-02 11:37:45 +0100

    Use default (smaller) model and keep session history off by default
    gpt4 can be used by using the aliasses with an l in the name. (`airfl`)
    session can be started with `aiStartSession`. See whether already in shell with `aiShowSession`

--- unicornsh.zsh
+++ unicornsh.zsh
@@ -1,19 +1,18 @@
 # If AI enabled, add a unicorn in front of the prompt
-PS1="$(if [[ -n $SCRIPT ]]; then echo '🦄 '; fi)""$PS1"
+PS1="$(if [[ -n $SCRIPT ]]; then echo '🦄 '; fi)$PS1"
 
 export PATH=$PATH:~/.unicornsh/scripts
 
 # Pipe the context into the sgpt command
- alias ai='~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions $(if [[ -n $AISESSION ]]; then echo "--chat $AISESSION";
- fi)'
- alias ais='~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions --shell $(if [[ -n $AISESSION ]]; then echo "--chat
- $AISESSION"; fi)'
- alias air='~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions --repl temp $(if [[ -n $AISESSION ]]; then echo "--ch
- $AISESSION"; fi)'
- alias airf='~/.unicornsh/scripts/aicontext.sh | sgpt --repl temp $(if [[ -n $AISESSION ]]; then echo "--chat $AISESSION"
- fi)'
- alias airfl='~/.unicornsh/scripts/aicontext.sh 500 | sgpt --repl temp $(if [[ -n $AISESSION ]]; then echo "--chat
- $AISESSION"; fi)'
+alias ai='~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions $(if [[ -n $AISESSION ]]; then echo "--chat $AISESSION"; fi)'
+alias ais='~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions --shell $(if [[ -n $AISESSION ]]; then echo "--chat $AISESSION"; fi)'
+alias air='~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
+alias airf='~/.unicornsh/scripts/aicontext.sh | sgpt --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
+alias airfl='~/.unicornsh/scripts/aicontext.sh | sgpt --model gpt-4-turbo-preview --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
+alias airfo='~/.unicornsh/scripts/aicontext.sh 500 | sgpt --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
+alias airflo='~/.unicornsh/scripts/aicontext.sh 500 | sgpt --model gpt-4-turbo-preview --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
+
+
 # alias ai="~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions"
 # alias ais="~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions --shell"
 # alias air="~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions --repl temp"
@@ -23,3 +22,4 @@ export PATH=$PATH:~/.unicornsh/scripts
 ### Other tools
 alias aiStartSession='export AISESSION=$(date -u +"%Y-%m-%dT%H:%M:%SZ")'
 alias aiStopSession='unset AISESSION'
+alias aiShowSession='echo "AISESSION is set to $AISESSION"'
