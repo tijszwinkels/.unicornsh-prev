447da1ad6657ba9852b873f7718283f964b45b6a
Tijs Zwinkels <tijs@tinkertank.eu>
2024-02-16 23:39:48 +0100

    Better parsing of last command output + other improvements

--- scripts/aicontext.sh
+++ scripts/aicontext.sh
@@ -9,13 +9,14 @@ LASTOUTPUTLINES=${1:-50}
 
 echo "The last command I executed was (including shell prompt):"
 echo '```'
-# SCRIPT contains the current shell log. Reverse with tail -r. 
-tail -r $SCRIPT | grep --max-count 2 "🦄" | tail -n 1
+# Use tail -r to reverse the lines, grep to find the prompt, and process the output
+tail -r $SCRIPT | grep --max-count=2 -e "^🦄" | tail -n 1
 echo '```'
 echo ""
 
 echo "$LASTOUTPUTLINES last lines of output of last executed command:"
 echo '```'
-tail -r $SCRIPT | grep --max-count 2 -B 10000 "🦄" |  tail -r | grep --invert-match "🦄" | tail -n $LASTOUTPUTLINES
+tail -r $SCRIPT | gsed 's/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]//g' | grep --max-count=2 -B 10000 -e "^🦄" | tail -r | grep --invert-match -e "^🦄" | tail -n $LASTOUTPUTLINES
 echo '```'
 echo ""
+
--- unicornsh.zsh
+++ unicornsh.zsh
@@ -1,16 +1,26 @@
 # If AI enabled, add a unicorn in front of the prompt
-PS1="$(if [[ -n $SCRIPT ]]; then echo '🦄 '; fi)$PS1"
+PS1="$(if [[ -n $SCRIPT ]]; then echo '\n🦄'; fi)$PS1"
 
 export PATH=$PATH:~/.unicornsh/scripts
 
 # Pipe the context into the sgpt command
+# ai = sgpt
+# s = shell mode
+# r = repl mode
+# f = function calling
+# l = OpenAI gpt4 instead of default model. Consider running a local model for privacy!
+# o = Use last 500 lines of output from the shell instead of the default 50
+
 alias ai='~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions $(if [[ -n $AISESSION ]]; then echo "--chat $AISESSION"; fi)'
+alias ail='~/.unicornsh/scripts/aicontext.sh | sgpt --model openai/gpt-4-turbo-preview --no-functions $(if [[ -n $AISESSION ]]; then echo "--chat $AISESSION"; fi)'
 alias ais='~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions --shell $(if [[ -n $AISESSION ]]; then echo "--chat $AISESSION"; fi)'
+alias aif='aif "fix this"'
 alias air='~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
+alias airl='~/.unicornsh/scripts/aicontext.sh | sgpt --model openai/gpt-4-turbo-preview --no-functions --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
 alias airf='~/.unicornsh/scripts/aicontext.sh | sgpt --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
-alias airfl='~/.unicornsh/scripts/aicontext.sh | sgpt --model gpt-4-turbo-preview --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
+alias airfl='~/.unicornsh/scripts/aicontext.sh | sgpt --model openai/gpt-4-turbo-preview --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
 alias airfo='~/.unicornsh/scripts/aicontext.sh 500 | sgpt --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
-alias airflo='~/.unicornsh/scripts/aicontext.sh 500 | sgpt --model gpt-4-turbo-preview --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
+alias airflo='~/.unicornsh/scripts/aicontext.sh 500 | sgpt --model openai/gpt-4-turbo-preview --repl $(if [[ -n $AISESSION ]]; then echo "$AISESSION"; else echo "temp"; fi)'
 
 
 # alias ai="~/.unicornsh/scripts/aicontext.sh | sgpt --no-functions"
